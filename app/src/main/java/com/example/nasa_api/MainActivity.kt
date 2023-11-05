ae7bca32ec42abbee398cfbda3605dfdb53b9ff2
Rian Corcino <riancorci@gmail.com>
2023-11-04 17:18:43 -0700

    connected json object data to xml values

--- app/src/main/java/com/example/nasa_api/MainActivity.kt
+++ app/src/main/java/com/example/nasa_api/MainActivity.kt
@@ -6,15 +6,16 @@ import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import com.bumptech.glide.Glide
 import com.codepath.asynchttpclient.AsyncHttpClient
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
 import okhttp3.Headers
 import java.security.MessageDigest
 
 class MainActivity : AppCompatActivity() {
-    val apikey = "c4ZzyyrOj83uEMRgyWzvWA3dmoU1RSzDiBTvYIHD"
+    val apikey = "9iQlN2XMftiNNSFuReywkEMZLHxZGoH0E5uBxffL"
     val timestamp = System.currentTimeMillis()
-    val baseurl = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos"
+    val baseurl = "https://api.nasa.gov/planetary/apod"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,11 +25,13 @@ class MainActivity : AppCompatActivity() {
         val text1 = findViewById<TextView>(R.id.text1)
         val text2 = findViewById<TextView>(R.id.text2)
         val next = findViewById<Button>(R.id.next)
+
+        getNextData(pic, text1, text2, next)
     }
 
     private fun getNextData(pic: ImageView, t1: TextView, t2: TextView, next: Button) {
         next.setOnClickListener{
-            val randId = (0..1000).random() //change later
+            val randId = (0..9).random() //change later
             getURL(pic, t1, t2, randId)
         }
     }
@@ -37,24 +40,45 @@ class MainActivity : AppCompatActivity() {
         val client = AsyncHttpClient()
 
         //need to include timestamp later?
-        val url ="$baseurl?sol=$randnum&api_key=$apikey"
+        val url ="$baseurl?api_key=$apikey&count=10" //also try without count
+
+        Log.d("getURL()", "function called")
 
         client[url, object : JsonHttpResponseHandler() {
             override fun onSuccess(statusCode: Int, headers: Headers?, json: JsonHttpResponseHandler.JSON) {
                 Log.d("NASA", "response successful $json")
-//                t1.text =
+
+                if (json != null && json.jsonArray != null) {
+                    val jsonArray = json.jsonArray
+                    val obj = jsonArray.getJSONObject(randnum)
+                    val img = obj.getString("url")
+                    val title = obj.getString("title")
+                    val explanation = obj.getString("explanation")
+
+                    t1.text = title
+                    t2.text = explanation
+
+                    loadImage(img, pic)
+                }
             }
 
             override fun onFailure(
                 statusCode: Int,
                 headers: Headers?,
-                response: String?,
+                response: String,
                 throwable: Throwable?
             ) {
-                TODO("Not yet implemented")
+                Log.d("NASA Error", response)
             }
 
         }]
+    }
+
+    private fun loadImage(imageUrl: String, imageView: ImageView) {
+        Glide.with(this)
+            .load(imageUrl)
+            .fitCenter()
+            .into(imageView)
     }
 
     private fun generateHash(timestamp: Long): String {
